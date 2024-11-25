57616d118478f05948e2ea5027fc1e6dc55ecd2e
Casper Howse <casperhowse@gmail.com>
2024-11-25 15:18:33 +1300

    Change a whole lot of things (I forgor)

--- backend/app.ts
+++ backend/app.ts
@@ -1,40 +1,42 @@
+import { walk } from "@std/fs";
+
+async function getTheFile(filePath:string):Promise<string> {
+    if (filePath == "/") return "index.html";
+
+    const filePaths = [];
+    for await (const walkEntry of walk("../website")) {
+        if (walkEntry.isFile) filePaths.push(walkEntry.path.replaceAll("\\", "/").replace("../website", ""));
+    }
+
+    if (filePaths.includes(filePath + ".html")) return filePath + ".html";
+
+    if (filePaths.includes(filePath)) return filePath;
+
+    return "404.html";
+}
+
 async function handler(req: Request) {
-    const url = new URL(req.url);
-    let pathname = url.pathname;
+    const reqURL = new URL(req.url);
+    let reqPath = reqURL.pathname;
 
-    if (pathname.startsWith("/api")) {
-        pathname = pathname.replace("/api", "");
+    if (reqPath.startsWith("/api")) {
+        reqPath = reqPath.replace("/api", "");
 
-        if (pathname.startsWith("/status")) {
-            pathname = pathname.replace("/status", "");
+        if (reqPath.startsWith("/status")) {
+            reqPath = reqPath.replace("/status", "");
 
             return new Response("gup (This means that cappabot.com is up and running)");
         }
-        return new Response(`API request to ${pathname}`);
+        return new Response(`API request to ${reqPath}`);
     }
 
-    const filepath = decodeURIComponent(pathname);
-    console.log(filepath);
+    const reqFilePath = decodeURIComponent(reqPath);
 
-    try {
-        const htmlNames = [];
-        for await (const name of Deno.readDir("../webiste")) {
-            console.log(name);
-            htmlNames.push(name);
-        }
-        console.log(htmlNames);
-        
-        try {
-            const file = await Deno.open("../website" + filepath, { read: true });
-            return new Response(file.readable);
-        } catch {
-            const file = await Deno.open("../website" + filepath + ".html", { read: true });
-            return new Response(file.readable);
-        }
-    } catch {
-        const file = await Deno.open("../website/404.html", { read: true });
-        return new Response(file.readable, { status: 404 });
-    }
+    const resFileName = await getTheFile(reqFilePath);
+    const resStatus = resFileName == "404.html" ? 400 : 200;
+    
+    const file = await Deno.open("../website/" + resFileName);
+    return new Response(file.readable, { status: resStatus });
 }
 
 Deno.serve(handler);
